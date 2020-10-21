f50ecb9c9d3b75422f76013830337a49986a30fe
Roman Martynenko <rmartynenko@deloitte.com>
2020-10-21 10:37:39 +0300

    Fixed db queries

--- components/PokemonTypeMutiselect/hooks.js
+++ components/PokemonTypeMutiselect/hooks.js
@@ -1,19 +1,19 @@
-import { useQuery, usePage } from 'startupjs';
+import { useQuery, usePage } from 'startupjs'
 
 export const useSelect = () => {
-  const [selectedItems = [], $selectedItems] = usePage('selectedItems');
+  const [selectedItems = [], $selectedItems] = usePage('selectedItems')
   const [items = []] = useQuery('pokemons', {
     $aggregate: [
-      { $match: { _type: { $ne: null } } },
+      { $match: { type: { $exists: true } } },
       { $group: { _id: '$type' } },
       { $set: { name: '$_id' } }
     ]
-  });
+  })
 
   const setSelectedItems = (type, selected) =>
     selected
       ? $selectedItems.push(type)
-      : $selectedItems.set(selectedItems.filter(item => item !== type));
+      : $selectedItems.set(selectedItems.filter(item => item !== type))
 
-  return [items, selectedItems, setSelectedItems];
-};
+  return [items, selectedItems, setSelectedItems]
+}
--- components/PokemonsPagination/index.js
+++ components/PokemonsPagination/index.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import { observer, usePage, useQuery } from 'startupjs';
-import { Pagination } from '@startupjs/ui';
+import React from 'react'
+import { observer, usePage, useQuery } from 'startupjs'
+import { Pagination } from '@startupjs/ui'
 
-export const PAGE_SIZE = 10;
+export const PAGE_SIZE = 10
 
 const PokemonsPagination = observer(() => {
-  const [page = 0, $page] = usePage('page');
-  const [search] = usePage('search');
-  const [selectedItems = []] = usePage('selectedItems');
+  const [page = 0, $page] = usePage('page')
+  const [search] = usePage('search')
+  const [selectedItems = []] = usePage('selectedItems')
   const $match = {
-    _type: { $exists: true },
-    name: { $regex: search, $options: 'i' }
-  };
+    _type: { $exists: true }
+
+  }
+  if (search) {
+    $match.name = { $regex: search, $options: 'i' }
+  }
   if (selectedItems.length) {
-    $match.type = { $in: selectedItems };
+    $match.type = { $in: selectedItems }
   }
   const [[{ count = 0 } = {}]] = useQuery('pokemons', {
     $aggregate: [{ $match }, { $count: 'count' }]
-  });
+  })
 
   return pug`
     Pagination(
@@ -26,7 +29,7 @@ const PokemonsPagination = observer(() => {
       pages=Math.ceil(count / PAGE_SIZE)
       onChangePage=newPage => $page.set(newPage)
     )
-  `;
-});
+  `
+})
 
-export default PokemonsPagination;
+export default PokemonsPagination
